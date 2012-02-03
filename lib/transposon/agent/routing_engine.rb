90e90d280024542eea3c01ce843ba6b06b788932
Justin Lynn <justinlynn@gmail.com>
2012-02-03 14:12:46 -0800

    start of an interface for the routing engine

--- lib/transposon/agent/routing_engine.rb
+++ lib/transposon/agent/routing_engine.rb
@@ -5,6 +5,13 @@ module Transposon
     # the message should be passed next, in order to get the message closer
     # to its resource endpoint destination.
     class RoutingEngine
+      
+      # The route table currently used by the RoutingEngine to route messages
+      attr_reader :route_table
+      
+      # Return the resource to which the given message should be delivered next
+      def route message
+      end
     end
   end
 end
--- spec/unit/transposon/agent/routing_engine_spec.rb
+++ spec/unit/transposon/agent/routing_engine_spec.rb
@@ -1,4 +1,24 @@
 require 'unit/helper'
 
 describe Transposon::Agent::RoutingEngine do
+  
+  it 'is a class' do
+    described_class.should be_a(Class)
+  end
+  
+  describe 'an instance' do
+    
+    it 'has a route table' do
+      subject.should respond_to(:route_table)
+    end
+    
+    it 'responds to #route' do
+      subject.should respond_to(:route)
+    end
+    
+    context 'routing a message' do
+    
+    end
+    
+  end
 end
